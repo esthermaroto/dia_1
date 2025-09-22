26af065bc531648917fea218261e4f4b0be2268a
esthermaroto <esther.maroto.torres@gmail.com>
2025-09-22 12:48:25 +0200

    mvc movies

--- movies.php
+++ movies.php
@@ -1,16 +1,23 @@
 <?php
 //lista de peliculas enlazadas a la descripción de cada una
+$db= new mysqli("localhost","root","","test",3306);
+var_dump($db);
+
 class Movie{
     private $title;
     private $author;
     private $year;
     private $description;
+    private $poster;
+    private $id;
 
-    public function __construct($title,$author,$year,$description){
+    public function __construct($title,$poster,$author,$year,$description,$id){
         $this->title = $title;
         $this->description = $description;
         $this->author = $author;
         $this->year = $year;
+        $this->poster = $poster;
+        $this->id = $id;
     }
 
     public function getTitle(){
@@ -26,11 +33,18 @@ class Movie{
     public function getYear(){
         return $this->year;
     }
+    public function getPoster(){
+        return $this->poster;
+    }
+    public function getId(){
+        return $this->id;
+    }
+
 }
 
-$movies[]= new Movie("El Padrino","Francis Ford Coppola",1972,"La historia de la familia Corleone, una de las más poderosas familias mafiosas de Nueva York.");
-$movies[]= new Movie("Pulp Fiction","Quentin Tarantino",1994,"La historia de varios personajes interconectados en Los Ángeles, incluyendo a dos asesinos a sueldo, un boxeador y una pareja de ladrones.");
-$movies[]= new Movie("Inception","Christopher Nolan",2010,"Un ladrón que tiene la habilidad de entrar en los sueños de las personas y robar sus secretos más profundos es contratado para realizar un último trabajo: implantar una idea en la mente de un objetivo.");
+$movies[]= new Movie("El Padrino","foto.png","Francis Ford Coppola",1972,"La historia de la familia Corleone, una de las más poderosas familias mafiosas de Nueva York.",1);
+$movies[]= new Movie("Pulp Fiction","foto.png","Quentin Tarantino",1994,"La historia de varios personajes interconectados en Los Ángeles, incluyendo a dos asesinos a sueldo, un boxeador y una pareja de ladrones.",2);
+$movies[]= new Movie("Inception","foto.png","Christopher Nolan",2010,"Un ladrón que tiene la habilidad de entrar en los sueños de las personas y robar sus secretos más profundos es contratado para realizar un último trabajo: implantar una idea en la mente de un objetivo.",3);
 
 
 
@@ -47,17 +61,19 @@ $movies[]= new Movie("Inception","Christopher Nolan",2010,"Un ladrón que tiene 
 <body>
     <h1>Películas</h1>
     <?php
-    if(!isset($_GET['title'])){
+    if(!isset($_GET['id'])){
             foreach($movies as $movie){
-                echo "<a href='index.php?title=".$movie->getTitle()."'>" . $movie->getTitle() . "</a><br>";
+                echo "<a href='index.php?id=".$movie->getId()."'>" . $movie->getTitle() . "</a><br>";
             }
     }else{
             foreach ($movies as $movie) {
-                if ($movie->getTitle() == $_GET['title']) {
+                if ($movie->getId() == $_GET['id']) {
                     echo "<h2>" . $movie->getTitle() . "</h2>";
                     echo "<p><strong>Director:</strong> " . $movie->getAuthor() . "</p>";
                     echo "<p><strong>Año:</strong> " . $movie->getYear() . "</p>";
                     echo "<p><strong>Descripción:</strong> " . $movie->getDescription() . "</p>";
+                    echo "<img src='posters/" . $movie->getPoster() . "' alt='" . $movie->getTitle() . " Poster' style='width:200px;'><br>";
+                    echo "<br><a href='index.php'>Volver a la lista de películas</a>";
                 }
             }
     }
@@ -66,3 +82,5 @@ $movies[]= new Movie("Inception","Christopher Nolan",2010,"Un ladrón que tiene 
     ?>
 </body>
 </html>
+
+
---
+++ mvcMovie/controllers/mainController.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once('models/Movie.php');
+
+//cargar modelo
+$db = Connection::connect();
+$q='SELECT * FROM peliculas';
+$result = $db->query($q);
+if($result){
+    while($row = mysqli_fetch_array($result))
+    {
+        echo $row['title'];
+        //cambiarlo para crearlo como objeto 
+        //$movies[]= new Movie($row['title'],$row['poster'],$row['author'],$row['year'],$row['description'],$row['id']);
+
+    
+    }
+}
+//comprobar variables
+
+//operar
+$movies[]= new Movie("El Padrino","foto.png","Francis Ford Coppola",1972,"La historia de la familia Corleone, una de las más poderosas familias mafiosas de Nueva York.",1);
+$movies[]= new Movie("Pulp Fiction","foto.png","Quentin Tarantino",1994,"La historia de varios personajes interconectados en Los Ángeles, incluyendo a dos asesinos a sueldo, un boxeador y una pareja de ladrones.",2);
+$movies[]= new Movie("Inception","foto.png","Christopher Nolan",2010,"Un ladrón que tiene la habilidad de entrar en los sueños de las personas y robar sus secretos más profundos es contratado para realizar un último trabajo: implantar una idea en la mente de un objetivo.",3);
+
+// cargar vista
+if(!isset($_GET['id'])){
+           require_once 'views/mainView.phtml';
+    }else{
+            require_once 'views/moviesView.phtml';
+    }
+
+?>
